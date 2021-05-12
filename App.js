fccbd25124dcd479284034e1b38ecc5300939974
vladkaprelev <vladkaprelev@gmail.com>
2021-05-12 09:57:51 +0700

    refactoring

--- App.js
+++ App.js
@@ -3,14 +3,6 @@ import NavigationService from './src/Navigation/NavigationService';
 import {Provider} from 'react-redux';
 import store from './src/Redux/CreateStore';
 
-global._fetch = fetch;
-global.fetch = function (uri, options, ...args) {
-  return global._fetch(uri, options, ...args).then(response => {
-    console.log('Fetch', {request: {uri, options, ...args}, response});
-    return response;
-  });
-};
-
 const App = () => {
   return (
     <Provider store={store}>
